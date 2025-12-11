9afbe86eb4847a4456707ca2da65005f66488a71
catalin-sbora <catalin.sbora@gmail.com>
2025-12-11 09:04:21 -0800

    Add SecurityTestController with ping endpoint
    Added SecurityTestController exposing GET api/SecurityTest/{ip} endpoint. The endpoint runs a ping command for the given IP using a new process and returns a static response. Controller uses [ApiController] and [Route] attributes for API routing.

---
+++ BankApplication/BankApplication/Controllers/SecurityTestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityTestController : ControllerBase
+    {
+    
+
+        // GET api/<SecurityTestController>/5
+        [HttpGet("{ip}")]
+        public string Get(string ip)
+        {
+            var cmd = "ping -c " + ip;
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "-c \"" + cmd + "\"");
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.UseShellExecute = false;
+            Process? process = Process.Start(processStartInfo);
+            process?.WaitForExit();
+            return "value";
+        }
+
+    
+    }
+}
